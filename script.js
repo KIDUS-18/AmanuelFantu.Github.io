63189cba4a9d45253ab4a248eb45b0e6640a5a8c
KIDUS-18 <95223482+KIDUS-18@users.noreply.github.com>
2021-12-08 06:35:43 -0800

    AmanuelFantu.github.io
    AmanuelFantu.github.io

---
+++ script.js
@@ -0,0 +1,40 @@
+const showBtn = document.querySelector('.navBtn');
+const topNav = document.querySelector('.top-nav');
+
+showBtn.addEventListener('click', function(){
+    if(topNav.classList.contains('showNav')){
+        topNav.classList.remove('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
+    } else {
+        topNav.classList.add('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-times"></i>';
+    }
+});
+
+/* Lightbox */
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+let sortBtn = document.querySelector('.filter-menu').children;
+let sortItem = document.querySelector('.filter-item').children;
+
+for(let i = 0; i < sortBtn.length; i++){
+    sortBtn[i].addEventListener('click', function(){
+        for(let j = 0; j< sortBtn.length; j++){
+            sortBtn[j].classList.remove('current');
+        }
+
+        this.classList.add('current');
+        
+
+        let targetData = this.getAttribute('data-target');
+
+        for(let k = 0; k < sortItem.length; k++){
+            sortItem[k].classList.remove('active');
+            sortItem[k].classList.add('delete');
+
+            if(sortItem[k].getAttribute('data-item') == targetData || targetData == "all"){
+                sortItem[k].classList.remove('delete');
+                sortItem[k].classList.add('active');
+            }
+        }
+    });
+}
